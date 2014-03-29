c4caff4fd7fa3e298d4e18bd1ee30aa183ed294b
lobojason@gmail.com <stfrans1983>
2014-03-29 18:58:57 -0400

    committing and ready with mySQL integration

--- Kwick.Api/App_Start/NinjectWebCommon.cs
+++ Kwick.Api/App_Start/NinjectWebCommon.cs
@@ -1,3 +1,10 @@
+using System.Web.Http;
+using FluentNHibernate.Cfg.Db;
+using Hack.Common;
+using NHibernate;
+using NHibernate.Context;
+using Ninject.Activation;
+
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Kwick.Api.App_Start.NinjectWebCommon), "Start")]
 [assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(Kwick.Api.App_Start.NinjectWebCommon), "Stop")]
 
@@ -62,7 +69,103 @@ namespace Kwick.Api.App_Start
         private static void RegisterServices(IKernel kernel)
         {
             var containerConfigurator = new NinjectConfigurator();
-            containerConfigurator.Configure(kernel);
-        }        
+            Configure(kernel);
+        }
+
+        public static void Configure(IKernel container)
+        {
+            //Add all bindings/dependencies
+            AddBindings(container);
+
+            //Use the container and our NinjectDependencyResolver as
+            //application's resolver
+            var resolver = new NinjectDependencyResolver(container);
+            GlobalConfiguration.Configuration.DependencyResolver = resolver;
+
+        }
+
+
+        /// <summary>
+        /// Add all bindings/dependencies to the container
+        /// </summary>
+        private static void AddBindings(IKernel container)
+        {
+            ConfigureNHibernate(container);
+            ConfigureLog4net(container);
+
+        }
+
+        /// <summary>
+        /// Set up log4net for this application, including putting it in the 
+        /// given container.
+        /// </summary>
+        private static void ConfigureLog4net(IKernel container)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            //var loggerForWebSite = LogManager.GetLogger("enTourWeb");
+            //container.Bind<ILog>().ToConstant(loggerForWebSite);
+        }
+
+        /// <summary>
+        /// Used to fetch the current thread's principal as 
+        /// an <see cref="IUserSession"/> object.
+        /// </summary>
+        //public IUserSession CreateUserSession(IContext arg)
+        //{
+        //    return new UserSession(Thread.CurrentPrincipal as GenericPrincipal);
+        //}
+
+
+        /// <summary>
+        /// Used to fetch the current thread's principal as 
+        /// an <see cref="IUserSession"/> object.
+        /// </summary>
+        //public IUserSession CreateUserSessionAccessToken(IContext arg)
+        //{
+        //    //var authenticatedUser = HttpContext.Current.User.Identity.Name;
+
+        //    return new UserSession(HttpContext.Current.User as GenericPrincipal);
+        //}
+
+        /// <summary>
+        /// Sets up NHibernate, and adds an ISessionFactory to the given
+        /// container.
+        /// </summary>
+        public static void ConfigureNHibernate(IKernel container)
+        {
+            var sessionFactory = FluentNHibernate
+                .Cfg.Fluently.Configure()
+                .Database(MySQLConfiguration.Standard.ConnectionString(
+                        c => c.FromConnectionStringWithKey("hackdb")))
+                .CurrentSessionContext("web")
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Kwick.MySql.KUserMap>())
+                .BuildSessionFactory();
+
+            // Add the ISessionFactory instance to the container
+            container.Bind<ISessionFactory>().ToConstant(sessionFactory);
+
+            // Configure a resolver method to be used for creating ISession objects
+            container.Bind<ISession>().ToMethod(CreateSession);
+
+            container.Bind<ICurrentSessionContextAdapter>().To<CurrentSessionContextAdapter>();
+        }
+
+        /// <summary>
+        /// Method used to create instances of ISession objects
+        /// and bind them to the HTTP context.
+        /// </summary>
+        public static ISession CreateSession(IContext context)
+        {
+            var sessionFactory = context.Kernel.Get<ISessionFactory>();
+            if (!CurrentSessionContext.HasBind(sessionFactory))
+            {
+                // Open new ISession and bind it to the current session context
+                var session = sessionFactory.OpenSession();
+                CurrentSessionContext.Bind(session);
+            }
+
+            return sessionFactory.GetCurrentSession();
+        }
+
     }
 }
---
+++ Kwick.Api/Controllers/UsersController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Kwick.Data;
+using NHibernate;
+
+namespace Kwick.Api.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private readonly ISession _session;
+
+        public UsersController(ISession session)
+        {
+            _session = session;
+        }
+
+        // GET api/values
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            var users = _session.QueryOver<KUser>().List().ToList();
+
+            var response = request.CreateResponse(HttpStatusCode.OK, users);
+            //response.Headers.Add("Location", '/api/users');
+            return response;
+
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
